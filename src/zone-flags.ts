b55e1cd954b9f510ceb7dad5309d500a419c91eb
Max Schulte <max.schulte@thinktecture.com>
2022-03-23 11:16:33 +0100

    feat: create labs

--- src/zone-flags.ts
+++ src/zone-flags.ts
@@ -1,3 +1,4 @@
-(window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
-(window as any).__zone_symbol__UNPATCHED_EVENTS = ['pointermove'];
+// LAB 2 Include zone.js flags
+// (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+// (window as any).__zone_symbol__UNPATCHED_EVENTS = ['pointermove'];
 
