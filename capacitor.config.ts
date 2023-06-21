803d8a948bd831fd691fd2c14c2845bcb401377c
Max Schulte <max.schulte@thinktecture.com>
2023-06-21 20:01:37 +0200

    chore: add ios

---
+++ capacitor.config.ts
@@ -0,0 +1,15 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.thinktecture.babylonNg',
+  appName: 'ng-babylonjs-simpel-integration',
+  webDir: 'dist/ng-babylonjs-simpel-integration',
+  server: {
+    androidScheme: 'https',
+    cleartext: true,
+    
+    
+  }
+};
+
+export default config;
